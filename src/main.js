148d25884ca119b31644e7a06ee260c949bcbacc
OeilShine <oeilshine@gmail.com>
2020-07-21 21:03:11 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import "./plugins/element.js";
 
-Vue.config.productionTip = false
+Vue.prototype.axios = axios;
+Vue.config.productionTip = false;
+
+import { Row, Button } from "element-ui";
+Vue.use(Row);
+Vue.use(Button);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
